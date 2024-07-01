955d1da483a3268c2ad8d10f8a0bac80c1cd88d2
Kai Hilbourne <kaihilbourne@umail.ucsb.edu>
2024-07-01 16:26:43 -0700

    Added more content to about me

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -2,6 +2,7 @@
 import '../App.css'
 import skiprock from '../images/skiprock.jpg'
 import george from '../images/sneorge.jpg'
+import swimming from '../images/swimming.JPG'
 
 export default function About(){
     return(
@@ -17,21 +18,40 @@ export default function About(){
                     />
                     <div className='items'>
                         <p>
-                            I grew up in Portland, Oregon, and I love the outdoors. Particularly, skipping rocks.
+                            I grew up in Portland, Oregon, and I love the outdoors. I particularly enjoy skipping rocks.
                         </p>
                         <p>
                             Swimming and jumping in rivers is one of my favorite things. 
                             I once did a flip off of a 40 foot jump, but I am far too scared to ever try that again.
                         </p>
                         <p>
-                            I also love hiking and birdwatching. I think Pinnacles National Park rocks, 
-                            not only because a California Condor flew 10 feet over my head.
+                            I also love hiking and birdwatching. Pinnacles National Park is the perfect place to do both of those things, 
+                            and it is where a California Condor flew just 10 feet above my head.
                         </p>
                         <p>
-                            I'm also kind of a nerd. I play Dungeons and Dragons and Magic the Gathering, and I love geeking out about game design and rules.
+                            I'm also kind of a nerd. I play the modern game of Magic the Gathering, 
+                            and the ancient game of Go, and I love geeking out about game design and rules interactions.
                         </p>
                         <p>
-                            
+                            I studied Mandarin for four years in high school, and I can still carry a decent conversation.
+                        </p>
+                        
+                    </div>
+                </div>
+            </div>
+            <div className='left-side'>
+                <div className='bodytext'>
+                    <img 
+                        className='items'
+                        src={swimming}
+                        height={"25%"} 
+                        width={"25%"}
+                        alt="Me swimming off a waterfall"
+                    />
+                    <div className='items'>
+                        <p>
+                            I was a competitive swimmer in high school. My best events were the 100 and 200 free, as well as 100 fly. I set multiple team records.
+                            As a senior in high school, I set up a mentorship program to help connect the team's younger swimmers with high school aged swimmers.
                         </p>
                         
                     </div>
